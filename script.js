eb81c91288dd550a2c416867533b1908365901e0
Slashin <trabbyoly@hotmail.com>
2017-07-23 17:09:42 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function myMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(43.589778, -79.659516),
-        zoom: 14,
-        mapTypeId: google.maps.MapTypeId.TERRAIN
-    }
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
